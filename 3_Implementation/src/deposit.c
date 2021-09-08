cb72de7187cd0f31a68aafb81bd25f53e794fc19
Tejaswini M D <89584926+TEJASWINIDODAMANI@users.noreply.github.com>
2021-09-08 20:06:24 +0530

    Create deposit.c

---
+++ 3_Implementation/src/deposit.c
@@ -0,0 +1,16 @@
+#include "header.h"
+
+int amountToDeposit,anotherTransaction;
+int deposit()
+{
+    printf("Please enter amount to deposit: ");
+    scanf("%d", &amountToDeposit);
+    int balance = amountToDeposit + balance;
+    printf("Thank you for depositing the amount, your new balance is: %f", balance);
+    printf("\n\nDo you want another transaction?\nPress 1 to proceed and 2 to exit\n\n");// request for another transaction
+    scanf("%d", &anotherTransaction);
+    if(anotherTransaction == 1)
+     {
+        ATM_Transaction(); // call our transaction method here
+     }
+}
