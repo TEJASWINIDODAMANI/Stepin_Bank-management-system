11b93651386607c64811653e3179316a94169805
Tejaswini M D <89584926+TEJASWINIDODAMANI@users.noreply.github.com>
2021-09-08 19:58:42 +0530

    Update and rename calculator_operations.h to header.h

---
+++ 3_Implementation/inc/header.h
@@ -0,0 +1,15 @@
+/** 
+* @file calculator_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __HEADER_H
+#define __HEADER_H
+
+#include "stdio.h"
+
+int balance();
+int withdraw();
+int deposit();
+
+#endif
