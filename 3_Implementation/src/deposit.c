f5d21065a70ee98a582788a5e4d551ae7c0cecb2
Tejaswini M D <89584926+TEJASWINIDODAMANI@users.noreply.github.com>
2021-09-08 20:13:23 +0530

    Update deposit.c

--- 3_Implementation/src/deposit.c
+++ 3_Implementation/src/deposit.c
@@ -6,7 +6,7 @@ int deposit()
     printf("Please enter amount to deposit: ");
     scanf("%d", &amountToDeposit);
     int balance = amountToDeposit + balance;
-    printf("Thank you for depositing the amount, your new balance is: %f", balance);
+    printf("Thank you for depositing the amount, your new balance is: %d", balance);
     printf("\n\nDo you want another transaction?\nPress 1 to proceed and 2 to exit\n\n");// request for another transaction
     scanf("%d", &anotherTransaction);
     if(anotherTransaction == 1)
